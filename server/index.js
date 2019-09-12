4705562cc54d348c4bb68f5391ad5cfb9fefd6b1
Zack Miller <hotbodyfitness@gmail.com>
2019-09-11 21:10:40 -0700

    Added last component and finished styling

--- server/index.js
+++ server/index.js
@@ -15,4 +15,8 @@ app.use(function(req, res, next) {
   next();
 });
 
+// app.get('/comments/:artist/:song', (req, res) => {
+//   request(`http://ec2-54-245-205-70.us-west-2.compute.amazonaws.com:3002/comments/${req.params.artist}/${req.params.song}`).pipe(res)
+// });
+
 app.listen(port, () => { console.log(`Proxy is listening on port ${port}`) });
